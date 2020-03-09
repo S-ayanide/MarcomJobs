fb095077e15416f37343868b24274b733864fa14
Sayan Mondal <sayanmondal342@gmail.com>
2020-03-09 14:15:13 +0530

    Added Accordion

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -6,53 +6,60 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
+import Accordion from 'react-bootstrap/Accordion'
 import { IoIosArrowDropdown } from 'react-icons/io'
 
 const Testimonial = ({testimonials}) => {    
 
+    
     const testimonialArr = testimonials.map((name,i) => {        
         return(
             <Row>
-            <Col className="testimonialItem"> 
-                <Row>
-                    <Col sm={2}>
-                        <div className="img">
-                            <Image className="popularImage" src={testimonials[i].userCompanyImage} rounded />                                
-                        </div>
-                    </Col>
-                    <Col sm={1}>
-                        <Image className="userImage" src="https://www.linuxtrainingacademy.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" roundedCircle />
-                    </Col>
-                    <Col sm={3}>
-                        <div className="userInfo">
-                            <p>{testimonials[i].username}</p>
-                            <p>{testimonials[i].userCompany}</p>
-                        </div>
-                    </Col>
-                    <Col sm={4}>
-                        <Card className="testimonialCard" border="light">
-                            <Card.Body>
-                                <Card.Title>{testimonials[i].userCompany}</Card.Title>
-                                <Card.Subtitle className="mb-2">
-                                    <Rater />
-                                </Card.Subtitle>                                    
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <IoIosArrowDropdown className="testimonialDropdown" size={25}/>
-                    </Col> 
-                </Row> 
-            </Col>                
+                <Accordion className="accordionTestimonial" defaultActiveKey="0">                
+                    <Accordion.Toggle as={Row} eventKey={i}>
+                        <Col className="testimonialItem">
+                            <Row>
+                                <Col sm={2}>
+                                    <div className="img">
+                                        <Image className="popularImage" src={testimonials[i].userCompanyImage} rounded />                                
+                                    </div>
+                                </Col>
+                                <Col sm={1}>
+                                    <Image className="userImage" src="https://www.linuxtrainingacademy.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" roundedCircle />
+                                </Col>
+                                <Col sm={3}>
+                                    <div className="userInfo">
+                                        <p>{testimonials[i].username}</p>
+                                        <p>{testimonials[i].userCompany}</p>
+                                    </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <Card className="testimonialCard" border="light">
+                                        <Card.Body>
+                                            <Card.Title>{testimonials[i].userCompany}</Card.Title>
+                                            <Card.Subtitle className="mb-2">
+                                                <Rater />
+                                            </Card.Subtitle>                                    
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col>
+                                    <IoIosArrowDropdown className="testimonialDropdown" size={25}/>
+                                </Col> 
+                            </Row> 
+                        </Col>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey={i}>
+                        <Card.Body>Hello! I'm the body</Card.Body>
+                    </Accordion.Collapse>                
+                </Accordion>
             </Row>
         )}
     )        
 
     return(      
         <Container>
-            
-                {testimonialArr}    
-            
+            {testimonialArr}    
         </Container>  
         
     );
